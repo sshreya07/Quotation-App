616266b614f0fe6f80585507509ddb50d08b4779
Shreya Gupta <gshreya0799@gmail.com>
2022-03-03 21:08:28 +0530

    updated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');     //syntax known as common js to import modules
 const connectDB = require('./config/db');
+const path = require('path');
 
 //initialise express in a variable app
 const app = express();
@@ -13,6 +14,14 @@ app.use(express.json());
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/thought', require('./routes/thought'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
